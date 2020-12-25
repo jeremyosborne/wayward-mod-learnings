902a1b5f37716f6822c19a00ea4662e31835e947
Jeremy Osborne <josborne@coursera.org>
2020-12-25 15:23:10 -0800

    trying out wayward modding

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wayward Mod Learnings
+
+A mod for learning how to build a [Wayward mod](https://github.com/WaywardGame/types/wiki/Prerequisites).
+
+## Dev Notes
+
+Steam location for `+mod` commands is `~/Library/Application\ Support/Steam/steamapps/common/Wayward/Wayward.app/Contents/MacOS/Electron`.
---
+++ src/Mod.ts
@@ -0,0 +1,29 @@
+import Mod from "mod/Mod"
+
+export default class WaywardModLearnings extends Mod {
+
+  // ---------------------
+  // Wayward hooks (begin)
+  public onInitialize() {
+   // log = this.getLog();
+  }
+
+  public onUninitialize() {
+
+  }
+
+	public onLoad() {
+   // log.info("Hello World!");
+  }
+
+  public onUnload() {
+   // log.info("Goodbye World!");
+	}
+
+  public onSave() {
+
+  }
+  // Wayward hooks (end)
+  // ---------------------
+
+}
